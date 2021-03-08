72ae40c41adce921ac4d2025cb64842755df76c8
Omarja21 <jarrahom21@gmail.com>
2021-03-08 17:07:32 +0200

    test

--- js/app.js
+++ js/app.js
@@ -28,13 +28,13 @@ let main=document.getElementById('test');
 //   }
 
 // };
-function CalculateConsumptionForArea(minCust, maxCust, avgCookiesCust, numberOfHours, totalOfCookies, cookiePerHour){
+function CalculateConsumptionForArea(minCust, maxCust, avgCookiesCust){
   this.minCust=minCust;
   this.maxCust=maxCust;
   this.avgCookiesCust=avgCookiesCust;
-  this.numberOfHours=numberOfHours;
-  this.totalOfCookies=totalOfCookies;
-  this.cookiePerHour=cookiePerHour;
+  this.numberOfHours=14;
+  this.totalOfCookies=0;
+  this.cookiePerHour=[];
   this.genRandom= function(){
     let random=Math.floor(Math.random()*(this.maxCust-this.minCust+1)+this.minCust);
     return random;
@@ -50,56 +50,5 @@ function CalculateConsumptionForArea(minCust, maxCust, avgCookiesCust, numberOfH
     }
   };
 }
-
-let main=document.getElementById('test');
-
-// let Seattle= {
-//   minCust: 23,
-//   maxCust: 65,
-//   avgCookiesCust:6.3,
-//   genRandom: function(){
-//     let random=Math.floor(Math.random()*(this.maxCust-this.minCust+1)+this.minCust);
-//     return random;
-//   },
-//   amountsOfCookiesPurchased: function(){
-//     let amount=this.avgCookiesCust*this.genRandom();
-//     return Math.floor(amount);
-//   },
-//   numberOfHours: 14,
-//   totalOfCookies:0,
-//   cookiePerHour: [],
-//   fillCookiePerHour: function(){
-//     for(let i=0; i<this.numberOfHours; i++){
-//       this.cookiePerHour[i]=this.amountsOfCookiesPurchased();
-//       this.totalOfCookies+=this.cookiePerHour[i];
-//     }
-//   },
-//   callConsumption: function(){
-//     this.fillCookiePerHour();
-//     consumptionPerHour('seattle', this.cookiePerHour, this.totalOfCookies);
-//   }
-
-// };
-function CalculateConsumptionForArea(minCust, maxCust, avgCookiesCust, numberOfHours, totalOfCookies, cookiePerHour){
-  this.minCust=minCust;
-  this.maxCust=maxCust;
-  this.avgCookiesCust=avgCookiesCust;
-  this.numberOfHours=numberOfHours;
-  this.totalOfCookies=totalOfCookies;
-  this.cookiePerHour=cookiePerHour;
-  this.genRandom= function(){
-    let random=Math.floor(Math.random()*(this.maxCust-this.minCust+1)+this.minCust);
-    return random;
-  };
-  this.amountsOfCookiesPurchased= function(){
-    let amount=this.avgCookiesCust*this.genRandom();
-    return Math.floor(amount);
-  };
-  this.fillCookiePerHour= function(){
-    for(let i=0; i<this.numberOfHours; i++){
-      this.cookiePerHour[i]=this.amountsOfCookiesPurchased();
-      this.totalOfCookies+=this.cookiePerHour[i];
-    }
-  };
-}
-
+let seattle= new CalculateConsumptionForArea(10,15,6.3);
+console.log(seattle.fillCookiePerHour());
