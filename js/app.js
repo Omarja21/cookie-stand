9411d0236a9215c50c413de0e2e2bf6550855bc4
Omarja21 <jarrahom21@gmail.com>
2021-03-11 00:18:35 +0200

    This is the first update for adding based on submitting

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 let main=document.getElementById('test');
 let table=document.getElementById('table');
 let hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let form=document.getElementById('Form');
 
 function CalculateConsumptionForArea(NameOfArea,minCust, maxCust, avgCookiesCust){
   this.NameOfArea=NameOfArea;
@@ -60,17 +61,30 @@ function CalculateConsumptionForArea(NameOfArea,minCust, maxCust, avgCookiesCust
     table.appendChild(trow);
   };
 }
+form.addEventListener('submit', testing);
+function testing(event){
+  event.preventDefault();
+  let NameOfArea= event.target.cityName.value;
+  let minCust=event.target.min.value;
+  let maxCust=event.target.max.value;
+  let avgCookiesCust=event.target.avg.value;
+  let newCity= new CalculateConsumptionForArea(NameOfArea, minCust, maxCust,avgCookiesCust);
+  newCity.tablebody();
+};
 
 let seattle= new CalculateConsumptionForArea('seattle',23,65,6.3);
 let Tokyo= new CalculateConsumptionForArea('Tokyo',3, 24, 1.2);
 let Dubai= new CalculateConsumptionForArea('Dubai',11,38,3.7);
 let Paris= new CalculateConsumptionForArea('Paris',20,38,2.3);
 let Lima= new CalculateConsumptionForArea('Lima',2,16,4.6);
+let aqaba=new CalculateConsumptionForArea('aqaba',20,25,3.3);
+let doha= new CalculateConsumptionForArea('Doha', 17,20,1.5);
+
 let totalCookiesOfEachCity=[];
-let city=[seattle, Tokyo, Dubai, Paris,Lima];
+let city=[seattle, Tokyo, Dubai, Paris,Lima,aqaba, doha];
 let totalForEachHourOfAllCities=[0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 let totalCookiesOfAllCities=0;
-for(let i=0;i<5; i++){
+for(let i=0;i<city.length; i++){
   city[i].fillCookiePerHour();
   totalCookiesOfEachCity[i]=city[i].totalOfCookies;
   totalCookiesOfAllCities+=city[i].totalOfCookies;
@@ -78,6 +92,7 @@ for(let i=0;i<5; i++){
     totalForEachHourOfAllCities[j]+= city[i].cookiePerHour[j];
   }
 }
+
 
 function tableHeaderMaker(){
   let trow=document.createElement('tr');
@@ -117,4 +132,7 @@ function makeTable(){
   tableFooterMaker();
 }
 makeTable();
+
+
+
 
